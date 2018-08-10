8810c77231a5d1be0136b7587c4e804a390b266b
James Peterson <jpeterson903@yahoo.com>
2018-08-10 11:41:35 -0700

    feat: add tests

---
+++ test/test.js
@@ -0,0 +1,48 @@
+const chai = require('chai')
+const should = chai.should()
+const app = require('../index')
+const request = require('supertest')
+
+describe('POST /api/file-metadata', () => {
+  it('should respond with image file metadata', done => {
+    request(app)
+      .post('/api/file-metadata')
+      .attach('testFile', `${__dirname}/sea.jpg`)
+      .end((err, res) => {
+        res.status.should.equal(200)
+        res.type.should.equal('application/json')
+        res.body.name.should.equal('sea.jpg')
+        res.body.size.should.equal(190307)
+        res.body.type.should.equal('image/jpeg')
+        done()
+    })
+  })
+
+  it('should respond with an error', done => {
+    request(app)
+      .post('/api/file-metadata')
+      .attach('testFile', `${__dirname}/paul-morris-212014.jpg`)
+      .end((err, res) => {
+        res.status.should.equal(400)
+        res.type.should.equal('application/json')
+        res.body.message.should.equal('File too large')
+        done()
+      })
+  })
+})
+
+describe('POST /api/file-metadata', () => {
+  it('should respond with text file metadata', done => {
+    request(app)
+      .post('/api/file-metadata')
+      .attach('testFile', `${__dirname}/test.txt`)
+      .end((err, res) => {
+        res.status.should.equal(200)
+        res.type.should.equal('application/json')
+        res.body.name.should.equal('test.txt')
+        res.body.type.should.equal('text/plain')
+        res.body.size.should.equal(3144)
+        done()
+      })
+  })
+})
